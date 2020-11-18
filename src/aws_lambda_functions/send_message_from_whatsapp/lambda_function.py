1da6701c520beae68393dea291f65799bfcf68b9
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-11-18 18:04:39 +0600

    BT-94

---
+++ src/aws_lambda_functions/send_message_from_whatsapp/lambda_function.py
@@ -0,0 +1,17 @@
+import logging
+
+
+logger = logging.getLogger(__name__)  # Create the logger with the specified name.
+logger.setLevel(logging.WARNING)  # Set the logging level of the logger.
+
+
+def lambda_handler(event, context):
+    """
+    :argument event: The AWS Lambda uses this parameter to pass in event data to the handler.
+    :argument context: The AWS Lambda uses this parameter to provide runtime information to your handler.
+    """
+    # Return the status code value of the request.
+    return {
+        "statusCode": 200
+    }
+
